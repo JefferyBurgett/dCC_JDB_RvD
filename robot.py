00ba4de897a4f3626c9687da41556b631274ab3c
JefferyBurgett <jeffery.burgett@gmail.com>
2022-06-22 17:45:41 -0500

    Added Robot Attack Logic

--- robot.py
+++ robot.py
@@ -5,3 +5,7 @@ class Robot:
         self.name = name
         self.health = 50
         self.active_weapon = Weapon()
+
+    def attack(self, dinosaur):
+        dinosaur.health -= self.active_weapon.attack_power
+        print(f'{self.name} shoots {dinosaur.name} with {self.active_weapon.name} for {self.active_weapon.attack_power} leaving them with {dinosaur.health} remaining')
