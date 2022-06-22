3c4b96579a04062d05126b77422b8f69f12033a2
JefferyBurgett <jeffery.burgett@gmail.com>
2022-06-22 17:10:32 -0500

    Added Dino Attack Logic

--- dinosaur.py
+++ dinosaur.py
@@ -1,9 +1,9 @@
-from sys import api_version
-from unicodedata import name
-
-
 class Dinosaur:
     def __init__(self, name, ap):
         self.name = name
         self.attack_power = ap
         self.health = 50
+
+    def attack(self, robot):
+        robot.health -= self.attack_power
+        print(f"{self.name} tail smashes {robot.name} for {self.attack_power} damage leaving {robot.name} with {robot.health} health remaining")
